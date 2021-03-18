77a7871f1b9992f6ac721cdfe1fb0c30376ceb18
andythebreaker <e24074724@mail.ncku.edu.tw>
2021-03-18 13:08:37 +0800

    把上一個舊的不能自動動的版本砍掉

--- Initialize_Swiper.js
+++ Initialize_Swiper.js
@@ -1,12 +1,16 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
+    slidesPerView: 4,
     spaceBetween: 30,
-    slidesPerGroup: 3,
+    slidesPerGroup: 4,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
+    },
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
     },
     navigation: {
       nextEl: '.swiper-button-next',
---
+++ index.js
@@ -0,0 +1,4 @@
+var card_boundaryS=document.getElementsByClassName("card_boundary");
+for (let card_boundaryE of card_boundaryS) {
+    card_boundaryE.classList.add("my-2");
+}
