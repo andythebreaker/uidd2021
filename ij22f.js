f14c7eac8ef193c8581126de64663d58251fb2b5
andythebreaker <43373581+andythebreaker@users.noreply.github.com>
2022-10-05 21:57:54 +0800

    Update ij22f.js

--- ij22f.js
+++ ij22f.js
@@ -1,3 +1,4 @@
+console.log("<<<ij>>>");
 window.onmessage = function(e) {
 console.log("https://www.folkstalk.com/2022/09/send-response-from-iframe-to-parent-with-code-examples.html");
 console.log(e.data);
